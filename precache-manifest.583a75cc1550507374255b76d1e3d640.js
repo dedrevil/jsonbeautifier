8f47dc25b88b95da1e9194d9ae48967b73235c2d
“Subash <schandran@axidio.com>
2020-05-30 09:18:10 +0530

    Updates

---
+++ precache-manifest.583a75cc1550507374255b76d1e3d640.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b3a7063114e7f5f5e91bbcf589d888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af2c322c5b251654590b",
+    "url": "/static/css/2.c4b89d91.chunk.css"
+  },
+  {
+    "revision": "8cf853ddc688f604b714",
+    "url": "/static/css/main.a2ad661d.chunk.css"
+  },
+  {
+    "revision": "af2c322c5b251654590b",
+    "url": "/static/js/2.2b6b5517.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2b6b5517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf853ddc688f604b714",
+    "url": "/static/js/main.60e6a244.chunk.js"
+  },
+  {
+    "revision": "ccf9b4b0839959021047",
+    "url": "/static/js/runtime-main.363b8662.js"
+  },
+  {
+    "revision": "90f87368e7ec09e1a2baaa17c3ca47ef",
+    "url": "/static/media/POOPCODE.90f87368.png"
+  }
+]);
